9a90141c5676ec38061d6691e4fb03821e1879ae
Pyrrho-elis <naolm75@gmail.com>
2025-08-02 23:34:01 +0300

    Update package version to 0.0.24 and export additional functions from nextHelpers in index.js.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const { getAllPosts } = require('./lib/getAllPosts');
 const { getPostBySlug } = require('./lib/getPostBySlug');
+const { getStaticPropsForPost, getStaticPathsForPosts } = require('./lib/nextHelpers');
 
 module.exports = {
     getAllPosts,
-    getPostBySlug
+    getPostBySlug,
+    getStaticPropsForPost,
+    getStaticPathsForPosts
 }
